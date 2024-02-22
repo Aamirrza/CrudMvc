6b341bef0b481b57a3ff07c392bb4762b1310541
KokaniJon <jonkokani@gmail.com>
2024-02-22 16:33:47 +0530

    Added Repo

---
+++ Repositories/IStudentRepository.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public interface IStudentRepository
+    {
+        void AddStudent(tblStudent student, IFormFile profileImg, IFormFile document);
+
+        void UpdateExistingStudent(tblStudent student, IFormFile profileImg, IFormFile document);
+        tblStudent FetchStudentDetails(int id);
+
+        public List<tblStudent> FetchAllStudents(int id);
+
+        void DeleteExistingStudent(int id);
+
+    }
+}
